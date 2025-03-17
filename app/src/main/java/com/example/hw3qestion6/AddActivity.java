3e42359b2565074082231f1a2c1f34ba9ab016fc
mjilani98 <mjilani@emich.edu>
2025-03-17 16:45:05 -0400

    Created AddActivity.java (controller) activity_Add.xml (View)

---
+++ app/src/main/java/com/example/hw3qestion6/AddActivity.java
@@ -0,0 +1,19 @@
+package com.example.hw3qestion6;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class AddActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //display Main screen
+        setContentView(R.layout.activity_add);
+
+    }
+}
